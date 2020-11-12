d67bb3b11a530ad82214704bd1e9970ebba833d1
Vitalii Tymofiichuk <vetalmoff@gmail.com>
2020-11-12 10:58:54 +0200

    add doctor_id to Appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,3 +1,5 @@
 class Appointment < ApplicationRecord
+  belongs_to :doctor
+  belongs_to :user
   validates :start_time, :end_time, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :appointments
+  has_many :doctors, through: :appointments
   validates :name, :email, presence: true
   validates :email, uniqueness: true
   has_secure_password
