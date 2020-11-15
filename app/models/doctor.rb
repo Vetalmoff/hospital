c56621f5a4eb783932898c6aaa1cc2f2193fc4ba
Vitalii Tymofiichuk <vetalmoff@gmail.com>
2020-11-16 01:33:48 +0200

    bootstrap buttons

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,6 @@
 class Doctor < ApplicationRecord
   has_many :appointments
   has_many :users, through: :appointments
+  validates :name, :img, :position, presence: true
+  validates :name, uniqueness: true
 end
