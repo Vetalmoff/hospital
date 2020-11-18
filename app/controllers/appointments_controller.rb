c8520b063ba1f49c2d49f737b43cca98ba3e4773
Vitalii Tymofiichuk <vetalmoff@gmail.com>
2020-11-18 15:38:42 +0200

    change color for yor appointment

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -17,18 +17,7 @@ class AppointmentsController < ApplicationController
   def new
     @appointment = Appointment.new
     if params[:doctor_id]
-      @appointments = Appointment.order(:start_time)
-
-      time_now = Time.now()
-      year = time_now.year
-      month = time_now.month
-      day = time_now.day
-
-      current_day = Time.new(year, month, day)
-      next_day = current_day + 1.day
-
-      @doctor = Doctor.find(params[:doctor_id])
-      @doctor_this_day_appointments = @appointments.where(["start_time > ? and start_time < ? and doctor_id = ?", current_day, next_day, params[:doctor_id]])
+      set_doctor_this_day_appointments
     end
   end
 
@@ -96,5 +85,21 @@ class AppointmentsController < ApplicationController
     def appointment_params
       params.require(:appointment).permit(:start_time, :end_time, :doctor_id, :user_id)
     end
+
+  def set_doctor_this_day_appointments
+    @appointments = Appointment.order(:start_time)
+
+    time_now = Time.now()
+    year = time_now.year
+    month = time_now.month
+    day = time_now.day
+
+    current_day = Time.new(year, month, day)
+    next_day = current_day + 1.day
+
+    @doctor = Doctor.find(params[:doctor_id])
+    @doctor_this_day_appointments = @appointments.where(["start_time > ? and start_time < ? and doctor_id = ?", current_day, next_day, params[:doctor_id]])
+
+  end
 end
 
--- app/controllers/certain_day_appointments_controller.rb
+++ app/controllers/certain_day_appointments_controller.rb
@@ -1,18 +1,18 @@
 class CertainDayAppointmentsController < ApplicationController
   def index
-    if params[:doctor_id]
-      @appointments = Appointment.order(:start_time)
+    @appointments = Appointment.order(:start_time)
 
-      time_now = Time.now()
-      year = time_now.year
-      month = time_now.month
-      day = time_now.day
+    time_now = Time.now()
+    year = time_now.year
+    month = time_now.month
+    day = time_now.day
 
-      current_day = Time.new(year, month, day)
-      next_day = current_day + 1.day
+    current_day = Time.new(year, month, day)
+    next_day = current_day + 1.day
 
-      @doctor = Doctor.find(params[:doctor_id])
-      @doctor_this_day_appointments = @appointments.where(["start_time > ? and start_time < ? and doctor_id = ?", current_day, next_day, params[:doctor_id]])
-    end
+    @doctor = Doctor.find(params[:doctor_id])
+    @doctor_this_day_appointments = @appointments.where(["start_time > ? and start_time < ? and doctor_id = ?", current_day, next_day, params[:doctor_id]])
+    @your_appointment = Appointment.where(["start_time > ? and start_time < ? and doctor_id = ? and user_id = ?", current_day, next_day, params[:doctor_id], params[:user_id]]).last
+
   end
 end
